e17aa4273371653aabd805a833917a6639d0c8b0
annie903318 <annie903318@gmail.com>
2022-07-17 17:57:31 +0800

    測試

--- eye_menu/menu_3.js
+++ eye_menu/menu_3.js
@@ -1,3 +1,14 @@
+const linebot = require('linebot');
+const https = require("https")
+const express = require("express")
+const app = express()
+
+const bot = linebot({
+    channelId: process.env.CHANNEL_ID,
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+
 function Menu3() {
   this.Ask_Msg = function(){
     let date = new Date();
@@ -7,14 +18,14 @@ function Menu3() {
     date.getDate() < 10?d = '0'+(date.getDate()):d = (date.getDate());
     let msg = {
         "type": "template",
-        "altText": "This is a buttons template",
+        "altText": "提醒事項",
         "template": {
             "type": "buttons",
             "text": "提醒事項",
             "actions": [
                 {
                   "type": "datetimepicker",
-                  "label": "紀錄事項",
+                  "label": "記錄事項",
                   "data": "type=1&menu=3",
                   "mode":"datetime",
                   "initial":`${date.getFullYear()}-${s}-${d}t00:00`,
@@ -84,57 +95,7 @@ function Menu3() {
           }
         });
       });
-  };
-  this.Schedule_Msg = function(client, event, pool){
-    client.pushMessage(event.source.userId, "msg");
-    // pool.connect(async function(err, pp, done){
-    //   let sql = `SELECT COUNT(userid) AS count FROM notes WHERE userid = '${event.source.userId}'`;
-    //   pp.query(sql, function(err, result){
-    //       // 存取資料用，將資料轉換成JSON
-    //       let count_results = { 'results': (result) ? result.rows : null};
-    //       // 拆解JSON資料
-    //       let count_s = JSON.parse(JSON.stringify(count_results));
-    //       // 抓取使用者全部的事項
-          
-    //         let sql = `SELECT id, description, m_date, m_time FROM notes WHERE userid = '${event.source.userId}'`;
-    //         pp.query(sql, function(err, result){
-    //           // 存取資料用，將資料轉換成JSON
-    //           let results = { 'results': (result) ? result.rows : null};
-    //           // 拆解JSON資料
-    //           let s = JSON.parse(JSON.stringify(results));
-    //           // 宣告星期陣列
-    //           let today = new Array("日","一","二","三","四","五","六");
-    //           // 用來存放用
-    //           let columns = [];
-    //           for(i = 0; i < sum; i++){
-    //             // 描述
-    //             let description = s.results[i].description;
-    //             // 日期
-    //             let now_date = s.results[i].m_date;
-    //             // 將日期格式化，轉成當天日期星期幾
-    //             let m_date = now_date.split('T')[0];
-    //             let now_week = new Date(Date.parse(m_date.replace(/-/g, '/')));
-    //             now_week = today[now_week.getDay()];
-    //             // 時間，判斷上午或下午
-    //             let now_time = s.results[i].m_time;
-    //             let a = now_time.split(':');
-    //             if(a[0] >= 0 && a[0] <= 11){
-    //               now_time = '上午 ' + now_time;
-    //             }
-    //             else if(a[0] >= 12 && a[0] <= 23){
-    //               now_time = '下午 ' + now_time;
-    //             }
-
-                
-    //           }
-              
-    //           pp.release();
-    //           return client.replyMessage(event.replyToken, msg);
-    //         });
-    //       }
-    //   );
-    // });
-  };
+  };  
   this.Cancel_Msg = function(client, event, pool){
     pool.connect(async function(err, pp, done){
       // 使用者狀態初始化
@@ -196,23 +157,25 @@ function Menu3() {
                   "header": {
                     "type": "box",
                     "layout": "horizontal",
+                    "backgroundColor": "#005757",
                     "contents": [
                       {
                         "type": "text",
                         "text": "待辦事項",
                         "weight": "bold",
-                        "color": "#00ff00",
+                        "color": "#ffffff",                          
                         "gravity": "center"
                       },
-                      {
-                        "type": "button",
-                        "action": {
-                          "type": "uri",
-                          "label": "設定提醒",
-                          "uri": "https://eye-secretary.herokuapp.com/auth/notify"
-                        },
-                        "height": "sm"
-                      }
+                      //原右上角設定提醒按鈕
+                      // {
+                      //   "type": "button",
+                      //   "action": {
+                      //     "type": "uri",
+                      //     "label": "設定提醒",
+                      //     "uri": "https://eye-secretary.herokuapp.com/auth/notify"
+                      //   },
+                      //   "height": "sm"
+                      // }
                     ],
                     "justifyContent": "center"
                   },
@@ -323,5 +286,55 @@ function Menu3() {
       });
     });
   }
+  this.Schedule_Msg = function(client, event, pool){
+    bot.push(event.source.userId, "msg");
+    // pool.connect(async function(err, pp, done){
+    //   let sql = `SELECT COUNT(userid) AS count FROM notes WHERE userid = '${event.source.userId}'`;
+    //   pp.query(sql, function(err, result){
+    //       // 存取資料用，將資料轉換成JSON
+    //       let count_results = { 'results': (result) ? result.rows : null};
+    //       // 拆解JSON資料
+    //       let count_s = JSON.parse(JSON.stringify(count_results));
+    //       // 抓取使用者全部的事項
+          
+    //         let sql = `SELECT id, description, m_date, m_time FROM notes WHERE userid = '${event.source.userId}'`;
+    //         pp.query(sql, function(err, result){
+    //           // 存取資料用，將資料轉換成JSON
+    //           let results = { 'results': (result) ? result.rows : null};
+    //           // 拆解JSON資料
+    //           let s = JSON.parse(JSON.stringify(results));
+    //           // 宣告星期陣列
+    //           let today = new Array("日","一","二","三","四","五","六");
+    //           // 用來存放用
+    //           let columns = [];
+    //           for(i = 0; i < sum; i++){
+    //             // 描述
+    //             let description = s.results[i].description;
+    //             // 日期
+    //             let now_date = s.results[i].m_date;
+    //             // 將日期格式化，轉成當天日期星期幾
+    //             let m_date = now_date.split('T')[0];
+    //             let now_week = new Date(Date.parse(m_date.replace(/-/g, '/')));
+    //             now_week = today[now_week.getDay()];
+    //             // 時間，判斷上午或下午
+    //             let now_time = s.results[i].m_time;
+    //             let a = now_time.split(':');
+    //             if(a[0] >= 0 && a[0] <= 11){
+    //               now_time = '上午 ' + now_time;
+    //             }
+    //             else if(a[0] >= 12 && a[0] <= 23){
+    //               now_time = '下午 ' + now_time;
+    //             }
+
+                
+    //           }
+              
+    //           pp.release();
+    //           return client.replyMessage(event.replyToken, msg);
+    //         });
+    //       }
+    //   );
+    // });
+  };
 };
 module.exports = Menu3; 
