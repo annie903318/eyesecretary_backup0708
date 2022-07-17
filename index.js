91f159047b09b6ae83406a323a51d495b4a3b902
annie903318 <annie903318@gmail.com>
2022-07-17 18:27:20 +0800

    測試

--- index.js
+++ index.js
@@ -429,7 +429,9 @@ function handlePostEvent(event){
     else if(_data.type == 1){
       // 紀錄
       ef3.Record_Msg(client, event, pool);
-      ef3.Schedule_Msg(client, event, pool);
+      setTimeout(function(){
+        ef3.Schedule_Msg(client, event, pool);
+      }, 5000);      
     }
     else if(_data.type == 2){
       // 查看
@@ -438,7 +440,9 @@ function handlePostEvent(event){
     else if(_data.type == 3){
       // 完成、刪除提醒事項
       ef3.Delete_Msg(client, event, pool, _data.m_id, _data.status);
-      ef3.Schedule_Msg(client, event, pool);
+      setTimeout(function(){
+        ef3.Schedule_Msg(client, event, pool);
+      }, 5000); 
     }
     
   }
