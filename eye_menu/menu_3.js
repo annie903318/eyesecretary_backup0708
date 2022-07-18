14de4921ebd311a041acc9821cb8b2378fb39cac
annie903318 <annie903318@gmail.com>
2022-07-19 03:10:03 +0800

    可推播，但會重複傳送

--- eye_menu/menu_3.js
+++ eye_menu/menu_3.js
@@ -306,11 +306,10 @@ function Menu3() {
             // 抓取使用者目前事項總共幾筆
             let sum = count_s.results[0].count;
             //清除全部排程
-            for (x = 0 ; x < sum ; x++) {
+            for (x = 0 ; x < timeouts.length ; x++) {
               clearTimeout(timeouts[x]); 
             }
-            timeouts = [];
-            console.log(timeouts);
+            timeouts = [];            
             //找尋全部的提醒事項並一一新增通知
             for(i = 0; i < sum; i++){
               // 描述
