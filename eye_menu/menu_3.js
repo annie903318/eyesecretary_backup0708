479b63af342a0e2e9c295e7dbc9571a6298c8528
annie903318 <annie903318@gmail.com>
2022-07-19 01:13:20 +0800

    可推播，但會重複傳送

--- eye_menu/menu_3.js
+++ eye_menu/menu_3.js
@@ -330,7 +330,7 @@ function Menu3() {
               // 現在日期
               let date = new Date().toLocaleString('zh-TW',{timeZone: 'Asia/Taipei',hour12: false});
               console.log('date',date);
-              date = new Date(date);
+              date = Date.parse(date);
               console.log('date',date);
               console.log('date',typeof(date));
               //計算通知時間與現在時間的時間差
